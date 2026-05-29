0917d0f150b3a4c1a105312792bafbddfa728810
Nofita Fitriyani <2311102001@ittelkom-pwt.ac.id>
2026-05-29 14:38:08 +0700

    dashboard pake dummy

--- frontend/src/components/layout/ProtectedRoute.jsx
+++ frontend/src/components/layout/ProtectedRoute.jsx
@@ -1,15 +1,5 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { Spinner } from '../ui/spinner';
-import { useAuth } from '../../hooks/useAuth';
+import { Outlet } from 'react-router-dom';
 
 export default function ProtectedRoute() {
-  const { isAuthenticated, isLoading } = useAuth();
-  if (isLoading) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <Spinner />
-      </div>
-    );
-  }
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+  return <Outlet />;
 }
