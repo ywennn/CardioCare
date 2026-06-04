d10387e52cc6e6b6701b242080fd27b410e12f33
ywennn <muhammadrobitzainulhaq@gmail.com>
2026-06-05 03:10:47 +0700

    tes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import server from './server/index.js';
 
 const port = process.env.PORT || 5000;
-
-server.listen(port, () => {
+const host = process.env.HOST || '0.0.0.0';
+server.listen(port, host, () => {
   console.log(`Server is running on port ${port}`);
 });
